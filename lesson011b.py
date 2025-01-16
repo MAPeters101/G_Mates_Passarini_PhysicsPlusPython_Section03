e5823a948e94d69ba20f59b8919789480a3a2cd4
Mark Peters <mapeters101@earthlink.net>
2025-01-16 17:34:39 -0500

    Work on lesson011b from 1:05 setup.

---
+++ lesson011b.py
@@ -0,0 +1,9 @@
+# The distance (m) and time (s) travelled by three projectiles were recorded in
+# an experiment.  Write a Python code to build a table that adequately displays
+# the data on the three projectiles: distance, time and velocity (that you must
+# obtain by the distance and the time travelled)
+# 202 m; 7.5s; v m/s
+# 14 m; 2.5s; v m/s
+# 312 m; 8.5s; v m/s
+
+
