0a9a5eb43f3d7f7cba21e69ee17025b1c88ce188
Mark Peters <mapeters101@earthlink.net>
2025-01-16 11:31:38 -0500

    Work on lesson010 to 5:00.

---
+++ lesson010.py
@@ -0,0 +1,35 @@
+a = 'I love programming'
+
+print(len(a))
+
+c = 17
+print(str(c))
+
+v = '14'
+print(float(v))
+
+print(a[7])
+
+print('I like Python,\nthis is a good language')
+print()
+
+name = 'William'
+age = 18
+
+print('My name is',name,'and I am ',age,'years old')
+print(f'My name is {name} and I am {age} years old')
+print()
+
+print(a[3:8])
+print(a[4:])
+print(a[:5])
+print()
+
+
+
+
+
+
+
+
+
