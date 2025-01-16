5d6e0482a969b9dace29409ff5ed15980fb24919
Mark Peters <mapeters101@earthlink.net>
2025-01-16 17:41:55 -0500

    Complete work on lesson011b from 1:05.

--- lesson011b.py
+++ lesson011b.py
@@ -6,4 +6,18 @@
 # 14 m; 2.5s; v m/s
 # 312 m; 8.5s; v m/s
 
+def velocity(d, t):
+    return d/t
 
+values = (
+    (202, 7.5),
+    (14, 2.5),
+    (312, 8.5)
+)
+
+print('-'*30)
+print(f"\tD(m)\tT(s)\tv(m/s")
+print('-'*30)
+for value in values:
+    print(f"\t{value[0]}\t\t{value[1]}\t\t{velocity(value[0],value[1]):.2f}")
+print('-'*30)
